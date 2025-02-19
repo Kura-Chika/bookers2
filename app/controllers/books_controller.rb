efa549c0aa1d4700cb48bea96de100060254f85a
EC2 Default User <ec2-user@ip-172-31-6-120.ap-northeast-1.compute.internal>
2025-02-19 08:20:52 +0000

    [Update]4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,15 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.new
+    @newbook = Book.new
     @books = Book.all
     @user = current_user
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    @newbook = Book.new(book_params)
+    @newbook.user_id = current_user.id
+    @newbook.save
+    redirect_to book_path(@newbook)
   end
 
   def show
